e866a28e5eacbdf75a6a692d7c5dd3d9a4a22066
Hushunkang <2752962035@qq.com>
2020-04-07 12:36:06 +0800

    讲师分页查询+多条件查询效果

--- vue-admin-template-master/src/views/edu/teacher/list.vue
+++ vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -1,6 +1,40 @@
 <template>
 <div class="app-container">
-      讲师列表
+<!-- 查询表单 -->
+<el-form :inline="true" class="demo-form-inline">
+<el-form-item>
+<el-input v-model="teacherQuery.name" placeholder="讲师名"/>
+</el-form-item>
+
+<el-form-item>
+<el-select v-model="teacherQuery.level" clearable placeholder="讲师头衔">
+    <el-option :value="1" label="高级讲师"/>
+    <el-option :value="2" label="首席讲师"/>
+</el-select>
+</el-form-item>
+
+<el-form-item label="添加时间">
+<el-date-picker
+    v-model="teacherQuery.begin"
+    type="datetime"
+    placeholder="选择开始时间"
+    value-format="yyyy-MM-dd HH:mm:ss"
+    default-time="00:00:00"
+/>
+</el-form-item>
+<el-form-item>
+<el-date-picker
+    v-model="teacherQuery.end"
+    type="datetime"
+    placeholder="选择截止时间"
+    value-format="yyyy-MM-dd HH:mm:ss"
+    default-time="00:00:00"
+/>
+</el-form-item>
+
+<el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+<el-button type="default" @click="resetData()">清空</el-button>
+</el-form>
 <!-- 表格 -->
 <el-table
   v-loading="listLoading"
